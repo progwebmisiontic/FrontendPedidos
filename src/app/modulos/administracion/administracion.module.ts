9d6e7f3eb437ab2239fb9981d983fc01f63228ec
Programación Web <programacion.web.misiontic.2022@gmail.com>
2021-05-19 15:24:57 -0500

    versión con módulos y componentes

---
+++ src/app/modulos/administracion/administracion.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdministracionRoutingModule } from './administracion-routing.module';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto/editar-producto.component';
+import { EliminarProductoComponent } from './productos/eliminar-producto/eliminar-producto.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
+
+
+@NgModule({
+  declarations: [
+    CrearPersonaComponent,
+    EditarPersonaComponent,
+    EliminarPersonaComponent,
+    BuscarPersonaComponent,
+    CrearProductoComponent,
+    EditarProductoComponent,
+    EliminarProductoComponent,
+    BuscarProductoComponent
+  ],
+  imports: [
+    CommonModule,
+    AdministracionRoutingModule
+  ]
+})
+export class AdministracionModule { }
