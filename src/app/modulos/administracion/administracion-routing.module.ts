3e54dacff4d4ecaf854e87bb957c95b238cd24e1
Programación Web <programacion.web.misiontic.2022@gmail.com>
2021-05-20 00:06:12 -0500

    Componentes de página maestra

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-persona',
+    component: CrearPersonaComponent
+  },
+  {
+    path: 'editar-persona',
+    component: EditarPersonaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
