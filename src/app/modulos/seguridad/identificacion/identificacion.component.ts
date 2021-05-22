8f24cc0215fb6afa0dd7cecb559e7a4e4cba11d3
Programación Web <programacion.web.misiontic.2022@gmail.com>
2021-05-21 19:07:50 -0500

    Hasta implementaciónd e login en servico de seguridad

--- src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
+const cryptoJS = require("cryptojs");
 
 @Component({
   selector: 'app-identificacion',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdentificacionComponent implements OnInit {
 
-  constructor() { }
+  fgValidador: FormGroup = this.fb.group({
+    'usuario': ['', [Validators.required, Validators.email]],
+    'clave': ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService) { }
 
   ngOnInit(): void {
+  }
+
+  IdentificarUsuario(){
+    let usuario = this.fgValidador.controls["usuario"].value;
+    let clave = this.fgValidador.controls["clave"].value;
+    let claveCifrada = cryptoJS.MD5(clave);  
+    this.servicioSeguridad.Identificar(usuario, claveCifrada).subscribe((datos:any) => {
+      // OK
+      alert("Datos correctos")
+    }, (error: any) => {
+      // KO
+      alert("Datos Inválidos")
+    })  
   }
 
 }
